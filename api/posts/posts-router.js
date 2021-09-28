bed72196db586e2342f93b98ad9ae07053d8e225
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-28 17:43:53 -0600

    completes MVP requirements

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -93,4 +93,24 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+router.get("/:id/comments", async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findById(id);
+
+  if (!post) {
+    res.status(404).json({
+      message: "The post with the specified ID does not exist"
+    });
+  } else {
+    try {
+      const comments = await Post.findPostComments(id);
+      res.status(200).json(comments);
+    } catch (err) {
+      res.status(500).json({
+        message: "The comments information could not be retrieved"
+      });
+    }
+  }
+});
+
 module.exports = router;
