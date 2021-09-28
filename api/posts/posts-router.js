2cf5a667f882a6b590b1d835f5f2228e7976ba43
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-28 16:26:05 -0600

    adds posts put-request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -49,4 +49,29 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const post = await Post.findById(id);
+  if (!post) {
+    res.status(404).json({
+      message: "The post with the specified ID does not exist"
+    });
+  } else if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post"
+    });
+  } else {
+    try {
+      const updatedPost = await Post.update(id, req.body);
+      res.status(200).json({ id: post.id, ...req.body });
+      console.log(updatedPost);
+    } catch (err) {
+      res.status(500).json({
+        message: "The post information could not be modified"
+      });
+    }
+  }
+});
+
 module.exports = router;
