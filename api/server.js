6f26690358d55504b236cdcd5ec10d7c7bed3e03
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-28 14:52:00 -0600

    adds get posts and get post by id requests

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
-// implement your server here
 const express = require("express");
+const postsRouter = require("./posts/posts-router");
 
 const server = express();
 server.use(express.json());
 
-// require your posts router and connect it here
+server.use("/api/posts", postsRouter);
 
 module.exports = server;
