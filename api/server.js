eb16abc06e7f9fff79f82f651586fd643ee677a6
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-28 13:08:51 -0600

    starts server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,9 @@
 // implement your server here
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
 // require your posts router and connect it here
+
+module.exports = server;
